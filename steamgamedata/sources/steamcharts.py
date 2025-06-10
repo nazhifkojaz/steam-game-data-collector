e0ee878c9a3e275bd14b72e3372997d1df7d7a26
nazhifkojaz <nazhifkojaz@gmail.com>
2025-06-10 18:10:12 +0700

    modify data return logic and simplify steamchart logic

--- steamgamedata/sources/steamcharts.py
+++ steamgamedata/sources/steamcharts.py
@@ -10,7 +10,7 @@ class SteamCharts:
         """
         self.base_url = "https://steamcharts.com/app/"
 
-    def do_request(self, appid: str) -> BeautifulSoup:
+    def _do_request(self, appid: str) -> BeautifulSoup:
         """Perform a GET request to the SteamCharts API and return the BeautifulSoup object.
         Args:
             appid (str): The appid of the game to fetch data for.
@@ -18,10 +18,9 @@ class SteamCharts:
         Returns:
             BeautifulSoup: The BeautifulSoup object containing the response content.
         """
-        url = f"{self.base_url}{appid}"
-        result = {}
-        result["appid"] = appid
 
+        url = f"{self.base_url}{appid}"
+        
         # define the headers to simulate a browser request
         headers = {
             "User-Agent": (
@@ -34,59 +33,89 @@ class SteamCharts:
         response = requests.get(url, headers=headers)
         response.encoding = "utf-8"
         if response.status_code != 200:
-            raise ConnectionError(f"Failed to connect to SteamCharts API. Status code: {response.status_code}")
+            # raise ConnectionError(f"Failed to connect to SteamCharts API. Status code: {response.status_code}")
+            return None
         
         return BeautifulSoup(response.text, "html.parser")
     
-    def _parse_game_name(self, soup: BeautifulSoup) -> str:
-        """Parse the game name from the BeautifulSoup object.
+    def get_game_data(self, appid: str) -> dict:
+        """Parse the game stats from the beautifulSoup object.
         Args:
-            soup (BeautifulSoup): The BeautifulSoup object containing the response content.
-
+            appid (str): The appid of the game to fetch data for.
+            
         Returns:
-            str: The parsed game name.
+            dict: The result containing status, game data, and any error message if applicable.
         """
-        game_name = soup.find("h1", id="app-title")
-        if not game_name:
-            raise ValueError("Failed to parse SteamCharts data. No game name found.")
+        result = {
+            "status": False,
+            "data": None,
+            "error": None
+        }
+
+        soup = self._do_request(appid)
+
+        if not soup:
+            # raise ValueError("Failed to fetch data from SteamCharts.")
+            result["error"] = "Failed to fetch data from SteamCharts."
+            return result
         
-        return game_name.text
-    
-    def _parse_game_data(self, soup: BeautifulSoup) -> dict:
-        """Parse the game data from the BeautifulSoup object.
-        Args:
-            soup (BeautifulSoup): The BeautifulSoup object containing the response content.
-
-        Returns:
-            dict: The parsed game data.
-        """
-        result = {}
-
         # get the part where it contains the 24 hour peak and all time peak data
         peak_data = soup.find_all("div", class_="app-stat")
         if not peak_data:
-            raise ValueError("Failed to parse SteamCharts data. No peak data found.")
+            # raise ValueError("Failed to parse SteamCharts data. No peak data found.")
+            result["error"] = "Failed to parse SteamCharts data. No peak data found."
+            return result
         
-        # get the 24 hour peak and all time peak data
-        result["active_player_24h"] = int(peak_data[1].find("span", class_="num").text)
-        result["peak_active_player_all_time"] = int(peak_data[2].find("span", class_="num").text)
-
+        result["status"] = True
+        result["data"] = {
+            "active_player_24h": int(peak_data[1].find("span", class_="num").text),
+            "peak_active_player_all_time": int(peak_data[2].find("span", class_="num").text)
+        }
         return result
     
-    def _parse_active_player_data(self, soup: BeautifulSoup) -> list:
-        """Parse the active player data from the BeautifulSoup object.
+    def get_active_player_data(self, appid: str) -> dict:
+        """Fetch active player data from SteamChart based on its appid.
         Args:
-            soup (BeautifulSoup): The BeautifulSoup object containing the response content.
+            appid (str): The appid of the game to fetch data for.
 
         Returns:
-            list: The parsed active player data.
+            dict: The result containing the status, appid, name, active player data, and any error message if applicable.
         """
-        result = []
+        result = {
+            "status": False,
+            "appid": appid,
+            "name": None,
+            "active_player_data": [],
+            "error": None
+        }
+
+        soup = self._do_request(appid)
+
+        if not soup:
+            # raise ValueError("Failed to fetch data from SteamCharts.")
+            result["error"] = "Failed to fetch data from SteamCharts."
+            return result
         
+        # get the game name, if they don't have a game name, add an error message.        
+        game_name = soup.find("h1", id="app-title")
+        if not game_name:
+            # raise ValueError("Failed to parse SteamCharts data. No game name found.")
+            result["error"] = "Failed to parse SteamCharts data. No game name found."
+        else:
+            # if we have a game name, set the game name in the result
+            result["name"] = game_name.text
+        
+        # get the player data table, if it also doesn't exist, extend the error message and return the empty result
         player_data_table = soup.find("table", class_="common-table")
         if not player_data_table:
-            raise ValueError("Failed to parse SteamCharts data. No player data table found.")
+            # raise ValueError("Failed to parse SteamCharts data. No player data table found.")
+            result["error"] = "Failed to parse SteamCharts data. No player data table found." if not result["error"] else result["error"] + " No player data table found."
+            return result
         
+        # if we have a player data table, set the status to True
+        result["status"] = True
+
+        # get the player data rows, skipping the first two header rows
         player_data_rows = player_data_table.find_all("tr")[2:]
 
         for row in player_data_rows:
@@ -94,7 +123,7 @@ class SteamCharts:
 
             month, avg_players, gain, percentage_gain, peak_players = cols[:5]
 
-            result.append(
+            result["active_player_data"].append(
                 {
                     "month": datetime.strptime(month, "%B %Y").strftime("%Y-%m"),
                     "avg_players": float(avg_players.replace(",", "")),
@@ -105,49 +134,3 @@ class SteamCharts:
             )
 
         return result
-    
-    def get_game_data(self, appid: str) -> dict:
-        """Fetch game data from SteamCharts based on appid.
-        Args:
-            appid (str): The appid of the game to fetch data for.
-
-        Returns:
-            dict: The game data from SteamCharts.
-        """
-        soup = self.do_request(appid)
-        return {
-            "appid": appid,
-            "name": self._parse_game_name(soup),
-            **self._parse_game_data(soup)
-        }
-    
-    def get_active_player_data(self, appid: str) -> list:
-        """Fetch active player data from SteamCharts based on appid.
-        Args:
-            appid (str): The appid of the game to fetch data for.
-
-        Returns:
-            list: The active player data from SteamCharts.
-        """
-        soup = self.do_request(appid)
-        return {
-            "appid": appid,
-            "name": self._parse_game_name(soup),
-            "active_player_data": self._parse_active_player_data(soup)
-        }
-    
-    def get_all_data(self, appid: str) -> dict:
-        """Fetch all game data from SteamCharts based on appid.
-        Args:
-            appid (str): The appid of the game to fetch data for.
-
-        Returns:
-            dict: The game data from SteamCharts.
-        """
-        soup = self.do_request(appid)
-        return {
-            "appid": appid,
-            "name": self._parse_game_name(soup),
-            **self._parse_game_data(soup),
-            "active_player_data": self._parse_active_player_data(soup)
-        }
