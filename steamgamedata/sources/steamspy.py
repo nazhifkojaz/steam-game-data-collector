df80bde378ccfca018c3e878f9407b1fcdd5f671
nazhifkojaz <nazhifkojaz@gmail.com>
2025-06-12 16:38:06 +0700

    refactor: refactor steamspy.py

--- steamgamedata/sources/steamspy.py
+++ steamgamedata/sources/steamspy.py
@@ -1,20 +1,22 @@
+from steamgamedata.sources.base import BaseSource, SourceResult
+
 import requests
 
 
-class SteamSpy:
+class SteamSpy(BaseSource):
     def __init__(self):
         """Initialize the SteamSpy with the base URL."""
         self.base_url = "https://steamspy.com/api.php"
 
-    def get_game_data(self, appid: str) -> dict:
+    def fetch(self, appid: str) -> SourceResult:
         """Fetch game data from SteamSpy based on appid.
         Args:
             appid (str): The appid of the game to fetch data for.
-
+            
         Returns:
-            dict: The status, game data, and any error message if applicable.
+            SourceResult: A dictionary containing the status, data, and any error message if applicable.
         """
-        result = {"status": False, "data": None, "error": None}
+        result: SourceResult = {"status": False, "data": None, "error": None}
 
         url = f"{self.base_url}?request=appdetails&appid={appid}"
         response = requests.get(url)
@@ -26,7 +28,7 @@ class SteamSpy:
             return result
 
         data = response.json()
-        if not data["name"]:
+        if not data.get("name"):
             # raise ValueError(f"Data for appid {appid} not found in SteamSpy.")
             result["error"] = f"Data for appid {appid} not found in SteamSpy."
             return result
@@ -34,7 +36,7 @@ class SteamSpy:
         result["status"] = True
         result["data"] = {
             "appid": data.get("appid", appid),
-            "name": data.get("name", ""),
+            # "name": data.get("name", None),
             "average_forever": data.get("average_forever", None),
             "average_2weeks": data.get("average_2weeks", None),
         }
