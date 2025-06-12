fe39af4d616f42c05032645f7777c880da93b207
nazhifkojaz <nazhifkojaz@gmail.com>
2025-06-12 17:36:39 +0700

    refactor: update __init__.py in sources/

--- steamgamedata/sources/__init__.py
+++ steamgamedata/sources/__init__.py
@@ -1,6 +1,14 @@
+from .base import BaseSource, SourceResult
 from .gamalytic import Gamalytic
 from .steam import Steam
 from .steamcharts import SteamCharts
 from .steamspy import SteamSpy
 
-__all__ = ["Gamalytic", "Steam", "SteamCharts", "SteamSpy"]
+__all__ = [
+    "BaseSource",
+    "Gamalytic",
+    "SourceResult",
+    "Steam",
+    "SteamCharts",
+    "SteamSpy",
+]
