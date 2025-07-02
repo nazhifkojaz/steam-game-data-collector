93170d1b5dd2a18147fbba0ce5c8593f07030d0e
nazhifkojaz <nazhifkojaz@gmail.com>
2025-07-02 18:59:28 +0700

    refactor: update __init__.py

--- steamgamedata/sources/__init__.py
+++ steamgamedata/sources/__init__.py
@@ -1,16 +1,16 @@
 from .base import BaseSource, SourceResult
 from .gamalytic import Gamalytic
 from .howlongtobeat import HowLongToBeat
-from .steam import Steam
 from .steamcharts import SteamCharts
 from .steamspy import SteamSpy
+from .steamstore import SteamStore
 
 __all__ = [
     "BaseSource",
     "Gamalytic",
     "HowLongToBeat",
     "SourceResult",
-    "Steam",
+    "SteamStore",
     "SteamCharts",
     "SteamSpy",
 ]
