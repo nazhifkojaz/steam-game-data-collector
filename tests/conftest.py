ac38972e0410520af0105c6ac5ea9d324dd87f7d
nazhifkojaz <nazhifkojaz@gmail.com>
2025-08-08 18:04:30 +0700

    test: update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -69,7 +69,7 @@ def gamalytic_success_response_data():
     # since we allow impartial result and return pre-defined labels, no need to cover everything
     return {
         "steamId": "12345",
-        "name": 'mock_name',
+        "name": 'Mock Game: The Adventure',
         "price": 12.34,
         "reviews": 1234,
         "reviewsSteam": 1234,
@@ -88,7 +88,7 @@ def hltb_success_response_data():
     "data": [
         {
         "game_id": 1234,
-        "game_name": "mock_name",
+        "game_name": "Mock Game: The Adventure",
         "game_type": "game"
         }
     ]
@@ -353,3 +353,390 @@ def steamcharts_error_response_player_data_table_incorrect_structure():
     """
     return data
 
+@pytest.fixture
+def steamspy_success_response_data():
+    return {
+        "appid": 12345,
+        "name": "Mock Game: The Adventure",
+        "positive": 1234,
+        "negative": 12,
+    }
+
+@pytest.fixture
+def steamspy_not_found_response_data():
+    return {
+        "appid": 12345,
+        "name": None,
+        "developer": "",
+        "publisher": "",
+        "positive": 0,
+        "negative": 0,
+    }
+
+@pytest.fixture
+def steamstore_success_response_data():
+    return {
+        "12345": {
+            "success": True,
+            "data": {
+                "type": "mock",
+                "name": "Mock Game: The Adventure",
+                "steam_appid": 12345,
+                "ratings": {
+                    "pegi": {"rating":"12", "descriptors": "Bad Language"}
+                }
+            }
+        }
+    }
+
+@pytest.fixture
+def steamstore_not_found_response_data():
+    return {
+        "12345": {
+            "success": False
+        }
+    }
+
+@pytest.fixture
+def usersummary_success_response_open_profile():
+    return {
+        "response": {
+            "players": [{
+                "steamid": "12345",
+                "communityvisibilitystate": 3,
+                "profilestate": 1,
+                "personaname": "Mock Player",
+                "profileurl": "https://mocksteam.com/profiles/12345",
+                "lastlogoff": 123456789,
+                "realname": "Mock Player The Third",
+                "timecreated": 123456789,
+                "loccountrycode": "MO",
+                "locstatecode": "CK",
+                "loccityid": 12,
+            }]
+        }
+    }
+
+@pytest.fixture
+def usersummary_success_response_closed_profile():
+    return {
+        "response": {
+            "players": [
+            {
+                "steamid": "12345",
+                "communityvisibilitystate": 1,
+                "profilestate": 1,
+                "personaname": "Private Mockو",
+                "profileurl": "https://mocksteam.com/profiles/12345",
+                "personastate": 0
+            }
+            ]
+        }
+    }
+
+@pytest.fixture
+def usersummary_not_found_response_data():
+    return {
+        "response":{
+            "players":[]
+        }
+    }
+
+@pytest.fixture
+def owned_games_exclude_free_response():
+    return {
+        "response": {
+            "game_count": 2,
+            "games": [
+            {
+                "appid": 12345,
+                "playtime_forever": 123
+            },
+            {
+                "appid": 23456,
+                "playtime_forever": 1234
+            },
+            ]
+        }
+    }
+
+@pytest.fixture
+def owned_games_include_free_response():
+    return {
+        "response": {
+            "game_count": 3,
+            "games": [
+            {
+                "appid": 12345,
+                "playtime_forever": 123
+            },
+            {
+                "appid": 23456,
+                "playtime_forever": 1234
+            },
+            {
+                "appid": 570, # free game / Dota 2
+                "playtime_forever": 12345
+            }
+            ]
+        }
+    }
+
+@pytest.fixture
+def owned_games_no_games_owned():
+    return {
+        "response": {}
+    }
+
+@pytest.fixture
+def owned_games_only_own_free_games():
+    return {
+        "response": {
+            "game_count": 1,
+            "games": [
+                {
+                    "appid": 570,
+                    "playtime_forever": 12345,
+                }
+            ]
+        }
+    }
+
+@pytest.fixture
+def recently_played_games_active_player_response_data():
+    return {
+        "response": {
+            "total_count": 2,
+            "games": [
+            {
+                "appid": 12345,
+                "name": "Mock Game",
+                "playtime_2weeks": 12,
+                "playtime_forever": 123,
+            },
+            {
+                "appid": 23456,
+                "name": "Mock Online",
+                "playtime_2weeks": 1,
+                "playtime_forever": 1234,
+            }
+            ]
+        }
+    }
+
+@pytest.fixture
+def recently_played_games_free_player_response_data():
+    return {
+        "response": {
+            "total_count": 1,
+            "games": [
+                {
+                    "appid": 570,
+                    "name": "Dota 2",
+                    "playtime_2weeks": 1234,
+                    "playtime_forever": 12345,
+                }
+            ]
+        }
+    }
+
+@pytest.fixture
+def recently_played_games_inactive_player_response_data():
+    return {
+        "response": {}
+    }
+
+@pytest.fixture
+def review_initial_page():
+    return {
+        "success": 1,
+        "query_summary": {
+            "num_reviews": 2,
+            "review_score": 5,
+            "review_score_desc": "Mostly Positive",
+            "total_positive": 2,
+            "total_negative": 2,
+            "total_reviews": 4
+        },
+        "reviews": [
+            {
+            "recommendationid": "1",
+            "author": {
+                "steamid": "1",
+                "num_games_owned": 1,
+                "num_reviews": 1,
+                "playtime_forever": 3,
+                "playtime_last_two_weeks": 0,
+                "playtime_at_review": 3,
+                "last_played": 12345
+            },
+            "language": "english",
+            "review": "mock review",
+            "voted_up": True,
+            },
+            {
+            "recommendationid": "2",
+            "author": {
+                "steamid": "2",
+                "num_games_owned": 1,
+                "num_reviews": 1,
+                "playtime_forever": 2,
+                "playtime_last_two_weeks": 1,
+                "playtime_at_review": 2,
+                "last_played": 12345
+            },
+            "language": "tchinese",
+            "review": "mock review but in tchinese",
+            "voted_up": False,
+            },
+        ],
+        "cursor": "nextcursor"
+    }
+
+@pytest.fixture
+def review_second_page():
+    return {
+        "success": 1,
+        "query_summary": {
+            "num_reviews": 2,
+        },
+        "reviews": [
+            {
+            "recommendationid": "3",
+            "author": {
+                "steamid": "3",
+                "num_games_owned": 1,
+                "num_reviews": 1,
+                "playtime_forever": 3,
+                "playtime_last_two_weeks": 0,
+                "playtime_at_review": 3,
+                "last_played": 12345,
+            },
+            "language": "english",
+            "review": "mock review",
+            "voted_up": True,
+            },
+            {
+            "recommendationid": "4",
+            "author": {
+                "steamid": "4",
+                "num_games_owned": 1,
+                "num_reviews": 1,
+                "playtime_forever": 2,
+                "playtime_last_two_weeks": 1,
+                "playtime_at_review": 2,
+                "last_played": 12345,
+            },
+            "language": "schinese",
+            "review": "another mock review",
+            "voted_up": False,
+            },
+        ],
+        "cursor": "nextcursor"
+    }
+
+@pytest.fixture
+def review_error_not_found_response():
+    return {
+        "success": 1,
+        "query_summary": {
+            "num_reviews": 0
+        },
+        "reviews": [],
+        "cursor": None
+    }
+
+@pytest.fixture
+def review_error_unsuccessful_response():
+    return {
+        "success": 0,
+        "query_summary": {
+            "num_reviews": 0
+        },
+        "reviews": [],
+        "cursor": None
+    }
+
+@pytest.fixture
+def review_empty_response():
+    return {
+        "success": 1,
+        "query_summary": {
+            "num_reviews": 0,
+            "review_score": 0,
+            "review_score_desc": "No user reviews",
+            "total_positive": 0,
+            "total_negative": 0,
+            "total_reviews": 0
+        },
+        "reviews": [],
+        "cursor": "*"
+    }
+
+@pytest.fixture
+def review_only_tchinese():
+    return {
+        "success": 1,
+        "query_summary": {
+            "num_reviews": 1,
+            "review_score": 0,
+            "total_positive": 0,
+            "total_negative": 1,
+            "total_reviews": 1
+        },
+        "reviews": [
+            {
+            "recommendationid": "2",
+            "author": {
+                "steamid": "2",
+                "num_games_owned": 1,
+                "num_reviews": 1,
+                "playtime_forever": 3,
+                "playtime_last_two_weeks": 0,
+                "playtime_at_review": 3,
+                "last_played": 12345
+            },
+            "language": "tchinese",
+            "review": "mock review but in tchinese",
+            "voted_up": False,
+            },
+        ],
+        "cursor": "*"
+    }
+
+@pytest.fixture
+def raw_data_normal():
+    # normal raw_data with correct data type
+    return {
+        "steam_appid": "12345",
+        "name": "Mock Game: The Adventure",
+        "developers": ["devmock_1", "devmock_2"],
+        "price_final": 12.34,
+        "owners": 1234,
+        "tags": ["RPG", "MOBA"],
+        "average_playtime_h": 1234,
+        "release_date": "Jan 1, 2025"
+    }
+
+@pytest.fixture
+def raw_data_invalid_types():
+    # raw_data with some invalid types
+    return {
+        "steam_appid": 23456, # should be a string,
+        "name": "mock game 2", # correct type
+        "developers": "devmock 3", # should be a list of string
+        "price_final": "12.34", # should be a float
+        "owners": "1234", # should be an integer
+        "tags": ["RPG", "MOBA"],
+        "release_date": "Not a date" # should be a None
+    }
+
+@pytest.fixture
+def raw_data_missing_steam_appid():
+    # I'm not sure if this is a valid case, but let's cover it
+    return {
+        "name": "mock game 3",
+        "developers": ["devmock 4"],
+        "price_final": 12.34,
+        "owners": 1234, # missing steam_appid
+    }
